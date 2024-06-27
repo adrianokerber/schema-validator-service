53920ce2d3a0ce786a20482da9ce7e1cd1109516
Adriano Kerber <kerberpro@gmail.com>
2024-06-27 09:59:34 -0300

    🎨 - Enhance error message formatting

--- src/SchemaValidator.HttpService/SchemaValidationContext/Domain/Schemas/SchemaValidation.cs
+++ src/SchemaValidator.HttpService/SchemaValidationContext/Domain/Schemas/SchemaValidation.cs
@@ -1,24 +1,51 @@
+using System.Text;
 using CSharpFunctionalExtensions;
 using NJsonSchema;
+using NJsonSchema.Validation;
 using SchemaValidator.HttpService.Shared;
 
 namespace SchemaValidator.HttpService.SchemaValidationContext.Domain.Schemas;
 
 public class SchemaValidation : IService<SchemaValidation>
 {
-    private const string ErrorTemplate = "Path: {0} Error: {1}";
+    private readonly JsonSchemaValidator _schemaValidator;
+
+    public SchemaValidation()
+    {
+        _schemaValidator = CreateSchemaValidator(false);
+    }
+
+    private JsonSchemaValidator CreateSchemaValidator(bool isCaseSensitive = true)
+    {
+        var comparerProperty = isCaseSensitive ? StringComparer.Ordinal : StringComparer.OrdinalIgnoreCase;
+        
+        var settings = new JsonSchemaValidatorSettings()
+        {
+            PropertyStringComparer = comparerProperty,
+        };
+
+        return new JsonSchemaValidator(settings);
+    }
     
     public Result Exec(JsonSchema schema, string json)
     {
-        var errors = schema.Validate(json);
+        var errors = _schemaValidator.Validate(json, schema);
         
-        Result result = Result.Success();
+        if (errors.Count == 0)
+            return Result.Success();
+
+        var errorMessage = FormatErrorMessageForMultipleErrors(errors);
+        return Result.Failure(errorMessage);
+    }
+
+    private string FormatErrorMessageForMultipleErrors(ICollection<ValidationError> errors)
+    {
+        var stringBuilder = new StringBuilder();
         foreach (var error in errors)
         {
-            var errorMessage = string.Format(ErrorTemplate, error.Path, error.Kind);
-            result = Result.Combine(" \n", result, Result.Failure(errorMessage));
+            stringBuilder.AppendFormat("Path: {0} Error: {1}\n ", error.Path, error.Kind);
         }
 
-        return result;
+        return stringBuilder.ToString();
     }
 }
